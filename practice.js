37c48a255696db185934fa71dd2894c437d3e927
Łukasz <qbaty44@gmail.com>
2022-11-18 16:48:26 +0100

    practice1.2

--- practice.js
+++ practice.js
@@ -1,6 +1,11 @@
 let todo1 = 99;
 
-let month = 'November';
-let dayOfMonth = "18,";
+let month = 'November ';
+let dayOfMonth = "18, ";
 let year = 2022;
 
+let number = 4+5*3;
+let number2 = (4+5)*3;
+
+let age = 28;
+let message = 'I am ' + age + ' years old';
