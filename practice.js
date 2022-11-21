845d92d09db740be14e22fee56a8d74dec71c77b
Łukasz <qbaty44@gmail.com>
2022-11-21 16:38:17 +0100

    pierwsze funkcje

--- practice.js
+++ practice.js
@@ -3,14 +3,37 @@ let todo2 = 'Wash car';
 let todo3 = 'Make dinner';
 
 
-let element = document.createElement('div');
-element.innerText = todo1;
-document.body.appendChild(element);
 
-element = document.createElement('div');
-element.innerText = todo2;
-document.body.appendChild(element);
+function addTodo(todoTitle) {
+    let element = document.createElement('div');
+    element.innerText = todoTitle;
+    document.body.appendChild(element);
+}
 
-element = document.createElement('div');
-element.innerText = todo3;
-document.body.appendChild(element);
+addTodo(todo1);
+addTodo(todo2);
+addTodo(todo3);
+
+
+let button = document.createElement('button')
+button.innerText = 'Click me';
+document.body.appendChild(button);
+
+
+document.title = 'ToDoApp';
+
+function greeting(firstName) {
+   console.log('Hello ' + firstName);
+}
+
+function toUpper(str) {
+    console.log(str.toUpperCase());
+}
+
+function convert(length) {
+    console.log(length * 2.54);
+}
+
+greeting('Łukasz');
+toUpper('Łukasz');
+convert(10);
