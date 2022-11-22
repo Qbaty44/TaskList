28641032a350d83740a2f159183c4f5dc0f7df29
Łukasz <qbaty44@gmail.com>
2022-11-22 16:00:14 +0100

    ćwiczenie na guzikach

--- practice.js
+++ practice.js
@@ -11,3 +11,33 @@ function addTodo() {
     let title = textbox.value;
     todos.push(title);
 }
+
+function changeToDone() {
+    let button = document.getElementById('todo-button');
+    button.innerText = 'Done';
+}
+
+let count = 0;
+function countUp () {
+    count = count +1;
+    updateCount();
+}
+
+function countDown() {
+    count = count -1;
+    updateCount();
+}
+
+function updateCount() {
+    let counter = document.getElementById('counter');
+    counter.innerText = count;
+}
+
+function displayTitle() {
+    let textbox = document.getElementById('todo-title');
+    let title = textbox.value;
+
+    let div = document.createElement('div');
+    div.innerText = title;
+    document.body.appendChild(div);
+}
