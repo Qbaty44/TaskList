5a4ccd03b8021b470d752512fb484d0ac707bca0
Łukasz <qbaty44@gmail.com>
2022-11-22 15:34:41 +0100

    textbox pierwsze podejście

--- practice.js
+++ practice.js
@@ -1,5 +1,4 @@
 let todos = ['Get groceries', 'Wash car', 'Make dinner'];
-todos.push('another todo');
 
 todos.forEach(function(todoTitle){
     let element = document.createElement('div');
@@ -7,41 +6,8 @@ todos.forEach(function(todoTitle){
     document.body.appendChild(element);
 });
 
-let button = document.createElement('button')
-button.innerText = 'Click me';
-document.body.appendChild(button);
-
-
-document.title = 'ToDoApp';
-
-function toUpper(stringArr) {
-    let result = [];
-    stringArr.forEach(function (str) {
-        result.push(str.toUpperCase());
-    });
-    console.log(result);
+function addTodo() {
+    let textbox = document.getElementById('todo-title');
+    let title = textbox.value;
+    todos.push(title);
 }
-toUpper(['hello ', 'world']);
-
-function arrayDouble(stringArr) {
-    let result = [];
-    stringArr.forEach(function (str) {
-        result.push(str);
-        result.push(str);
-    });
-    console.log(result);
-}
-
-arrayDouble(['bark ', 'meow ']);
-
-
-function arraySum(numArray) {
-    let total = 0;
-    numArray.forEach(function (num) {
-        total = total + num;
-    });
-    console.log(total);
-}
-
-arraySum([1, 2, 3, 4]);
-arraySum([-5, 8, 0, 4]);
